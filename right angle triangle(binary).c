0e265a3d9a945ae00a30b44de0eadae144e7301b
Deepak Menan R <92635140+Deepak-Menan-R@users.noreply.github.com>
2022-04-22 07:50:29 +0530

    Developed by Deepak Menan
    this file prints a right angle triangle with 0 and 1 .

---
+++ right angle triangle(binary).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main(){
+	int i,j,row;
+	printf("Enter number of rows to be printed : ");
+	scanf("%d",&row);
+	for(i=0;i<row;i++){
+		for(j=0;j<=i;j++){
+			if((i+j)%2==0){
+				printf("0");
+			}else{
+				printf("1");
+			}printf("\t");
+		}printf("\n");
+		
+	}
+}
